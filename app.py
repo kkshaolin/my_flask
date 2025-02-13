d9521f046fbb40950c0b62e60edc7230d0e3f723
Kittisak Keawpichai <6710110030@psu.ac.th>
2025-02-13 14:43:50 +0700

    add route regis

--- app.py
+++ app.py
@@ -16,10 +16,15 @@ class User(db.Model):
 
 with app.app_context():
     db.create_all()
-    
+
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
