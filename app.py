f2d5f02d53ed2e734888a48f27afc2ad91a746ba
Kittisak Keawpichai <6710110030@psu.ac.th>
2025-02-14 20:34:34 +0700

    re home page

--- app.py
+++ app.py
@@ -1,30 +1,31 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-
-with app.app_context():
-    db.create_all()
-
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route("/register")
 def register():
     return render_template("register.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/python1")
+def python1():
+    return render_template("python1.html")
+
+@app.route("/ex-python1")
+def ex_python1():
+    return render_template("ex-python1.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
