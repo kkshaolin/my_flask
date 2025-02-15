d4e766104ae9e2e918572745b4eb95b82af45d90
Kittisak Keawpichai <6710110030@psu.ac.th>
2025-02-15 18:23:49 +0700

    path python

--- app.py
+++ app.py
@@ -22,6 +22,26 @@ def dashboard():
 def python1():
     return render_template("learn/python1.html")
 
+@app.route("/python2")
+def python2():
+    return render_template("learn/python2.html")
+
+@app.route("/python3")
+def python3():
+    return render_template("learn/python3.html")
+
+@app.route("/python4")
+def python4():
+    return render_template("learn/python4.html")
+
+@app.route("/python5")
+def python5():
+    return render_template("learn/python5.html")
+
+@app.route("/python6")
+def python6():
+    return render_template("learn/python6.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
